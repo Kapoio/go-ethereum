3283d3901173e37fb07b088776912b38f1af0313
ivanshukhov <45999338+ivanshukhov@users.noreply.github.com>
2021-12-12 20:15:35 +0100

    params: update genesis

--- params/config.go
+++ params/config.go
@@ -27,7 +27,7 @@ import (
 
 // Genesis hashes to enforce below configs on.
 var (
-	MainnetGenesisHash = common.HexToHash("0xb3d57766745f504ba58d263f103f90ab135ff3c212a01d0fff55a315dc8d31e0")
+	MainnetGenesisHash = common.HexToHash("0x24831d32ece6a76a3daf44afe707cf5a20e26008191d4768e800e764104cf399")
 	RopstenGenesisHash = common.HexToHash("0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d")
 	SepoliaGenesisHash = common.HexToHash("0x25a5cc106eea7138acab33231d7160d69cb777ee0c2c553fcddf5138993e6dd9")
 	RinkebyGenesisHash = common.HexToHash("0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
