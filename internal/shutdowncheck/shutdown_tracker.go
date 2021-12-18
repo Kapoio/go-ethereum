ab5272f3fac4b1084ebe065b03ee62ddec6ab8d5
ivanshukhov <45999338+ivanshukhov@users.noreply.github.com>
2021-12-18 19:56:31 +0100

    internal: fix imports

--- internal/shutdowncheck/shutdown_tracker.go
+++ internal/shutdowncheck/shutdown_tracker.go
@@ -19,10 +19,10 @@ package shutdowncheck
 import (
 	"time"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/ethdb"
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/Kapoio/go-kapoio/common"
+	"github.com/Kapoio/go-kapoio/core/rawdb"
+	"github.com/Kapoio/go-kapoio/ethdb"
+	"github.com/Kapoio/go-kapoio/log"
 )
 
 // ShutdownTracker is a service that reports previous unclean shutdowns
